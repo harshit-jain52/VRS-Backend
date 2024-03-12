6e67d146a99ae7f5417493b792f1f54cc0e13b8c
harshit-jain52 <harshitjain0502@gmail.com>
2024-03-12 18:31:33 +0530

    Basic Login Routes

--- index.js
+++ index.js
@@ -5,11 +5,7 @@ const dotenv = require("dotenv");
 dotenv.config();
 const app = express();
 
-// connect to database
-mongoose
-  .connect(process.env.MONGODB_URI)
-  .then((result) => app.listen(process.env.PORT))
-  .catch((err) => console.log(err));
+app.listen(process.env.PORT);
 
 // register view engines
 app.engine("ejs", require("ejs").renderFile);
@@ -19,3 +15,21 @@ app.set("views", path.join(__dirname, "views"));
 // middleware & static files
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "public")));
+
+// Routes
+
+app.get("/", (req, res) => {
+  res.render("home", { title: "Home" });
+});
+
+app.get("/customer", (req, res) => {
+  res.render("customer_login", { title: "Customer" });
+});
+
+app.get("/staff", (req, res) => {
+  res.render("staff_login", { title: "Staff" });
+});
+
+app.get("/manager", (req, res) => {
+  res.render("manager_login", { title: "Manager" });
+});
